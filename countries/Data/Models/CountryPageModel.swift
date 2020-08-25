cd7bda38e0c88fecfaa408745a305cca4fc4cfcd
Кирилл Баюков <kirill.bayukov@appcraft.team>
2020-08-25 11:21:11 +0300

    networking models

---
+++ countries/Data/Models/CountryPageModel.swift
@@ -0,0 +1,14 @@
+//
+//  CountryPageModel.swift
+//  countries
+//
+//  Created by Кирилл Баюков on 25.08.2020.
+//  Copyright © 2020 bayukov. All rights reserved.
+//
+
+import Foundation
+
+class CountryPageModel {
+    var nextPage: URL?
+    var countries: [CountryModel] = []
+}
