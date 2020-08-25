95eb0e74d0f1f5b202a339fa625c89bcdee55f55
Кирилл Баюков <kirill.bayukov@appcraft.team>
2020-08-25 10:12:34 +0300

    initial commit

---
+++ countries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  countries
+//
+//  Created by Кирилл Баюков on 25.08.2020.
+//  Copyright © 2020 bayukov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
