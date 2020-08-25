04b2cb5da41c207b0bbc7205ae11213738cc73fb
Кирилл Баюков <kirill.bayukov@appcraft.team>
2020-08-25 10:57:09 +0300

    app base created

---
+++ countries/Screens/CountryList/CountryListEntity.swift
@@ -0,0 +1,13 @@
+//
+//  CountryListEntity.swift
+//  countries
+//
+//  Created by Кирилл Баюков on 25.08.2020.
+//  Copyright © 2020 bayukov. All rights reserved.
+//
+
+import Foundation
+
+class CountryListEntity {
+    
+}
---
+++ countries/Screens/CountryList/CountryListRouter.swift
@@ -0,0 +1,17 @@
+//
+//  CountryListRouter.swift
+//  countries
+//
+//  Created by Кирилл Баюков on 25.08.2020.
+//  Copyright © 2020 bayukov. All rights reserved.
+//
+
+import Foundation
+
+protocol CountryListRouterInput: AnyObject {
+    
+}
+
+class CountryListRouter: CountryListRouterInput {
+    
+}
