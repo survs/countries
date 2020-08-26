b39695548d3b8b8bab3d385ee499c18f75106113
Кирилл Баюков <kirill.bayukov@appcraft.team>
2020-08-26 10:21:20 +0300

    flag downloading

---
+++ countries/Networking/ImageDownloader.swift
@@ -0,0 +1,35 @@
+//
+//  ImageDownloader.swift
+//  countries
+//
+//  Created by Кирилл Баюков on 26.08.2020.
+//  Copyright © 2020 bayukov. All rights reserved.
+//
+
+import UIKit
+
+class ImageDownloader {
+    
+    class func downloadImages(urls: [URL], completion: @escaping([UIImage]) -> Void) {
+        DispatchQueue.global(qos: .default).async {
+            let group = DispatchGroup()
+            var images: [Any] = urls
+            for url in urls {
+                group.enter()
+                URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    if let data = data, let image = UIImage(data: data), let index = images.firstIndex(where: { ($0 as? URL)?.relativePath == response?.url?.relativePath }) {
+                        images.remove(at: index)
+                        images.insert(image, at: index)
+                    }
+                    group.leave()
+                }.resume()
+            }
+            group.notify(queue: .global()) {
+                images.removeAll(where: { !($0 is UIImage) })
+                guard let images = images as? [UIImage] else { return }
+                completion(images)
+            }
+        }
+    }
+    
+}
